e12d975cd64e303374fe372ea0347ea47e2365d4
Paul Woolcock <pwoolcock@tenable.com>
2017-07-31 14:56:55 -0400

    Add TokenType

---
+++ src/token_type.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+pub enum TokenType {
+    // single-character tokens
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+
+    // One or two character tokens
+    Bang, BangEqual,
+    Equal, EqualEquaL,
+    Greater, GreaterEqual,
+    Lesser, LesserEqual,
+
+    // Literals
+    Identifier, String, Number,
+
+    // Keywords
+    And, Class, Else, False, Fun, For, If, Nil, Or,
+    Print, Return, Super, This, True, Var, While,
+
+    Eof,
+}
