2f4946466bef0bfe18cd6da7920ecae6c7335f59
Paul Woolcock <pwoolcock@tenable.com>
2017-07-31 14:33:19 -0400

    Add error handling
    Adapted from the book to be more rust-like

---
+++ src/errors.rs
@@ -0,0 +1,14 @@
+error_chain!{
+    foreign_links {
+        ParseInt(::std::num::ParseIntError);
+        ParseFloat(::std::num::ParseFloatError);
+    }
+
+    errors {
+        LoxError(line: usize, msg: String) {
+            description("lox error")
+            display("[line: {}] Error: {}", line, msg)
+        }
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
+#[macro_use] extern crate error_chain;
+
 use std::fs::File;
 use std::io::{self, stdin, stdout, Read, Write, BufReader, BufRead};
+use std::process;
 
+pub use errors::*;
 use scanner::Scanner;
 use token::Token;
 
+mod errors;
 mod scanner;
 mod token;
 
@@ -12,7 +17,10 @@ pub fn run_file(filename: &str) {
     let mut src = String::new();
     let mut f = File::open(filename).unwrap();
     f.read_to_string(&mut src);
-    run(&src);
+    if let Err(e) = run(&src) {
+        eprintln!("{}", e.description());
+        process::exit(65);
+    }
 }
 
 pub fn run_prompt() {
@@ -31,17 +39,21 @@ pub fn run_prompt() {
             break;
         };
 
-        run(&line);
+        if let Err(e) = run(&line) {
+            eprintln!("{}", e.description());
+        }
     }
 }
 
-pub fn run(src: &str) {
+pub fn run(src: &str) -> Result<()> {
     let scanner = Scanner::new(src);
     let tokens = scanner.scan();
 
     for token in tokens {
         println!("{:?}", token);
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
